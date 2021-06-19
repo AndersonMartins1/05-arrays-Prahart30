85a1d6cd4c1cca2e78261dec7bd40a30699a25b8
Prahart30 <63900522+Prahart30@users.noreply.github.com>
2021-06-19 11:30:59 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,70 +1,71 @@
-/* Enter your solutions in this file */
 #include <stdio.h>
-int max(int a[], int n){
-      int large=a[0];
-    for(int i=0;i<n;i++){
-      if (a[i]>large)
-       large=a[i];
-     printf("max is %i \n",large);}
-       return large;
-       }
-int min(int a[], int n){
-      int small=a[0];
-    for(int i=0;i<n;i++){
-      if (small>a[i])
-       small=a[i];
-     printf("min is %i \n",small);}
-       return small;
-       }
-float average(int a[], int n){
-      int sum=0;
-      float avg;
-    for(int i=0;i<n;i++)
-      sum+=a[i];
-       avg=(float)sum/n;
-     printf("average is %f \n",avg);
-       return avg;
-       }
+#include <assert.h>
 
-   int mode(int a[], int n){
-      int c[n],count,large=c[0],i,no;
-    for( i=0;i<n;i++){
-        count=0;
-    for(int j=0;j<n;j++){
-      if(a[i]==a[j])
-         count++;
-         }
-       c[i]=count;
-       printf("%ith element of array c is %i \n",i,c[i]);}
-    for(i=0;i<n;i++){
-      if (c[i]>large)
-         no=i;
-        large=a[i];
-     printf("max is %i \n",large);
-     printf("%i\n",no);
-     printf("%i \n",a[i]);
-        return a[i];
+int max(int a[],int n){
+  int max= a[0];
+  for(int i=0; i<n; i++){
+    if(max < a[i]){
+    max=a[i];
+  }
+}
+return max;
+}
+
+
+int min(int b[], int n){
+  int min = b[0];
+  for(int i=0; i<n; i++){
+    if(min > b[i]){
+       min = b[i];
        }
-   }   
-int factors(int x, int a[]){
-  int i,j,count,num=0;
-  for(i=2;i<=x;i++){
-  count=0;
-    if((x%i==0)){
-    for(j=1;j<=i;j++){
-       if((i%j==0)){
-         count++;
-          }
        }
-    if(count<=2){
-        while(x%i==0){
-           a[num]=i;
-           x=x/i;
-           num++;
-          }
+       return min;
+       }
+       
+  
+       float average(int c[], int n){
+         float average =0;
+         for(int i=0; i<n; i++){
+           average+= c[i];
          }
-         }
-            
-            }
-     return num;
+         average = average/n;
+         return average;
+       }
+       
+       
+       int mode(int d[], int n){
+         int count = 0;
+         int mode = 0;
+         for(int i=0; i<n; i++){
+           int counts =0;
+           for(int j=0; j<n; j++){
+             if(d[j]==d[i]){
+               counts++;
+             }
            }
+           if(counts > count)
+             count=counts;
+           mode = d[i];
+         }
+          return mode;
+             }
+             
+             
+      int factors(int n,int e[]){
+        int i=0;
+        while(n%2==0){
+          e[i]=2;
+          i++;
+          n=n/2;
+        }
+        for(int j=3; j<=n;j++){
+          while(n%j==0){
+            e[i]=j;
+            i++;
+            n=n/j;
+          }
+        }
+        if(n>2)
+          e[i]=n;
+        return i;
+      }
