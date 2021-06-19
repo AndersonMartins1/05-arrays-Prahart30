05b2f1911b29d9412a3bec560219f06f6c1ce096
Prahart30 <63900522+Prahart30@users.noreply.github.com>
2021-06-19 11:36:29 +0530

    Update test_max.c

--- test_max.c
+++ test_max.c
@@ -2,15 +2,7 @@
 #include <assert.h>
 
 
-int max(int myarray[], int size) {
-    int c, maximum;
-    maximum = myarray[0];
-    for(c = 0; c < size; ++c) {
-        if(myarray[c] > maximum)
-        maximum = myarray[c];
-    }
-    return maximum;
-}
+int max(int [], int)
 
 int main(void) {
   int x[] = {9,5,6,10,2,-3,4};
